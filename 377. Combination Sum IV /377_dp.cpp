e9038a941ce55fd624e2a3309f1acc5ad90cfec9
tsai8890 <asdpeter6520@gmail.com>
2022-04-21 14:57:39 +0800

    4/21 LeetCode 377. [Combination Sum IV] Finished
    	377. Combination Sum IV
    		- Difficulty: Medium
    		- Algorithm: Dynamic Programming
    		    - Note:
    			- 這題要求的是所有combination+排序的數量，所以和一般的knapsack problem
    			  不一樣。
    
    			- 原本knapsack problem的transition是dp[i][amount] = dp[i-1][amount]
    			  + dp[i][amount-nums[i]]，即分成「結尾是nums[i]/結尾不是nums[i]」兩種情況。
    
    			  但是這題用這種做法會做不出來(當討論到結尾不是nums[i]的case時，
    			  不能直接加dp[i-1][amount]，因為結尾不是nums[i]，不代表不會用到nums[i]，
    			  dp[i-1][amount]代表只會用到nums[0..i-1])，所以直接加是不對的。
    
     			  這題比較簡潔的做法，是使用一維dp(因為討論只會用到nums[0..i]沒有用，每次
    			  都有可能用到nums[0..n-1]，所以只需要amount這個維度。
    
    			  dp[i]: 討論以nums[0], nums[1], ...nums[n-1]為結尾的所有組合即可
    			  Transition Function:
    				- dp[i] = sum(dp[i-nums[j]] for j in [0..n-1] if i >= nums[j]
    				- dp[0] = 1

--- 377. Combination Sum IV /377_dp.cpp
+++ 377. Combination Sum IV /377_dp.cpp
@@ -1,19 +1,28 @@
 #include <vector>
 using namespace std;
 
+/*
+    Title: 377. Combination Sum IV
+    Time Complexity: O(|target| * |nums|)
+    Space Complexity: O(|target|)-extra space
+
+    Algorithm: 
+        1. Dynamic Programming
+        
+    Note:
+        1. dp[i]: the number of possible sequences from nums
+           summing up to i
+        
+        2. Transition function:
+            i.  dp[i] = sum(dp[i-nums[j]]) for j in [0..n-1] if i >= nums[j]
+            ii. dp[0] = 1
+
+    Date: 04/21/2022 14:35									
+    Time: 0 ms  -- beat 100.00%    
+    Memory: 6.5 MB  -- beat 93.01% 
+*/
+
 class Solution {
-    /*
-        Title: 377. Combination Sum IV
-        Time Complexity: O(|target| * |nums|)
-        Algorithm: 
-            1. Dynamic Programming
-            2. dp[i]: the number of possible sequences from nums
-               summing up to i
-                        
-        Date: 12/06/2021 22:06	
-        Time: 4 ms  -- beat 35.87%
-        Memory: 6.6 MB  -- beat 64.21%
-    */
 public:
     int combinationSum4(vector<int>& nums, int target) {
         vector<unsigned int> dp(target + 1, 0);
