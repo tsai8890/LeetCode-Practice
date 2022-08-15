8881cbd047de191c49130ce398ac1af103348a68
peter <ninesun@TW-LW60551-M.local>
2022-08-15 15:25:16 +0800

    [Binary Tree] LC 429: Modify Codes
    - Add definition of TreeNode

--- sprint/binaryTree/429. N-ary Tree Level Order Traversal/429.cpp
+++ sprint/binaryTree/429. N-ary Tree Level Order Traversal/429.cpp
@@ -18,6 +18,27 @@ public:
 };
 */
 
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
