9a9da0f489cf395278177654af545c80881d2c9d
tsai8890 <asdpeter6520@gmail.com>
2022-09-14 06:25:50 +0800

    [UnionFind] LC 1254: Updated
    - (V) Rename

--- sprint/UnionFind/1254. Number of Closed Islands/1254-3.cpp
+++ sprint/UnionFind/1254. Number of Closed Islands/1254-3.cpp
@@ -22,7 +22,6 @@ public:
     int closedIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid.front().size();
-        int total = 0;
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < m; j ++) {
                 if (i == 0 || i == n-1 || j == 0 || j == m-1) {
