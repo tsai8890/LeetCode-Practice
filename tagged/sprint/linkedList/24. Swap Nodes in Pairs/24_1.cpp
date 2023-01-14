a807f701e019efc8b5889db1126cd9858e7231e1
tsai8890 <asdpeter6520@gmail.com>
2023-01-14 16:50:44 +0800

    [LinkedList] LC 24: Finished
    - (V) codes
    - (V) notes

---
+++ tagged/sprint/linkedList/24. Swap Nodes in Pairs/24_1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* prev = head;
+        ListNode* cur = head->next;
+        ListNode* next = head->next->next;
+
+        cur->next = prev;
+        prev->next = swapPairs(next);
+        return cur;
+    }
+};
